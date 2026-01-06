5a45383e4dfa997b9b5973f6e8263d2c48ead01a
clane83 <clane83@yahoo.com>
2026-01-05 18:38:22 -0800

    week1 teach updated for individual activity

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,32 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        List<int> results = new();
+        int h = 0;
+        int j = 0;
+        //Problem 2
+        for (int i = 0; i < select.Length; i++)
+        {
+
+            if (select[i] == 1)
+            {
+                results.Add(list1[h]);
+                h++;
+            }
+            else if (select[i] == 2)
+            {
+                results.Add(list2[j]);
+                j++;
+            }
+        }
+        return results.ToArray();
     }
 }
